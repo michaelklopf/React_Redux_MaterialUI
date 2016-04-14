67f950bb3b0ab17f354226b3c72e8c21a3951d19
Michael Klopf <klopf.michael@gmail.com>
2016-04-14 14:37:27 +0200

    Wrapper for the form, manifested in a smart form

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/*jshint esversion: 6 */
+// src/index.js
+// connect returns smart component build of React comp. current state and actions
+import React, {PropTypes} from 'react';
+import { createStore, applyMiddleware, compose} from 'redux';
+import { connect } from 'react-redux';
+import { Provider } from 'react-redux';
+import thunk from 'react-thunk';
+import createLogger from 'redux-logger';
+import Form from './components/Form';
+import * as actions from './actions';
+import store from './store';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const SmartForm = connect(state => state, actions)(Form);
+
+const reduxMiddleware = applyMiddleware(thunk, createLogger());
+
+export default props => (
+  <Provider store={compose(reduxMiddleware)(createStore)(store)}>
+    <SmartForm {...props} />
+  </Provider>
+);
